ba7c802d0c7cb87651934d25a9e4788d4f8a3d1f
atyrin <tyrin.andrew@gmail.com>
2017-02-01 23:23:22 +0300

    Fix mistake for charka and hachek. Replace for symbols in the middle of word.

--- logic.js
+++ logic.js
@@ -5,7 +5,7 @@ var data = `
 			"slovnik.seznam.cz" : "q"
 		},
 		"letters" : {
-			"hachek" : {
+			"charka" : {
 				"a" : "á",
 				"A" : "Á",
 				"o" : "ó",
@@ -19,7 +19,7 @@ var data = `
 				"i" : "í",
 				"I" : "Í"
 			},
-			"charka" : {
+			"hachek" : {
 				"e" : "ě",
 				"E" : "Ě",
 				"c" : "č",
@@ -48,17 +48,49 @@ var elem;
 Object.keys(json.sites).forEach(function(k){
     if (document.URL.includes(k)){
 			console.log("k: " + k);
-			elem = document.getElementById(json.sites[k]);
+			elem = document.getElementById(json.sites[k])
 			return;
 		}
 });
 
+
+
 elem.oninput = function(){
-	if(elem.value.length > 1){
+		var lastSymbolIndex = elem.value.search(["\'|\""])
+		console.log("Find \' or \" at: " + lastSymbolIndex);
+		if(lastSymbolIndex > 0){
+			if(elem.value[lastSymbolIndex] == "\'"){
+				repl(elem, json.letters.charka, lastSymbolIndex)
+			}
+			else{
+				repl(elem, json.letters.hachek, lastSymbolIndex)
+			}
+		}
+	}
+
+function repl(element, dict, index){
+	Object.keys(dict).forEach(function(k){
+		if (element.value[index-1] == k){
+			element.value = element.value.substring(0, index-1) + dict[k] + element.value.substring(index+1, element.value.length)
+			return;
+		}
+	});
+}
+/*
+		
 		var lastSymbol = elem.value[elem.value.length - 1];
 		var preLastSymbol = elem.value[elem.value.length - 2];
 		if (lastSymbol == "\'"){
 			console.log("Find spec symbol \'");
+			Object.keys(json.letters.charka).forEach(function(k){
+				if (preLastSymbol == k){
+					createNewValue(json.letters.charka[k]);
+					return;
+				}
+			});
+		}
+		else if(lastSymbol == "\""){
+			console.log("Find spec symbol \"")
 			Object.keys(json.letters.hachek).forEach(function(k){
 				if (preLastSymbol == k){
 					createNewValue(json.letters.hachek[k]);
@@ -66,11 +98,37 @@ elem.oninput = function(){
 				}
 			});
 		}
-		else if(lastSymbol == "\""){
-			console.log("Find spec symbol \"")
+	
+}
+
+
+
+
+
+
+
+elem.oninput = function(){
+	if(elem.value.length > 1){
+		var lastSymbolIndex = elem.value.search(["\'\""])
+		if(lastSymbolIndex > 0){}
+
+		
+		var lastSymbol = elem.value[elem.value.length - 1];
+		var preLastSymbol = elem.value[elem.value.length - 2];
+		if (lastSymbol == "\'"){
+			console.log("Find spec symbol \'");
 			Object.keys(json.letters.charka).forEach(function(k){
 				if (preLastSymbol == k){
 					createNewValue(json.letters.charka[k]);
+					return;
+				}
+			});
+		}
+		else if(lastSymbol == "\""){
+			console.log("Find spec symbol \"")
+			Object.keys(json.letters.hachek).forEach(function(k){
+				if (preLastSymbol == k){
+					createNewValue(json.letters.hachek[k]);
 					return;
 				}
 			});
@@ -82,3 +140,4 @@ function createNewValue(newAdditionalValue, element=elem){
 	console.log("Case: ", newAdditionalValue);
 	element.value = element.value.substring(0, element.value.length - 2) + newAdditionalValue;
 }
+*/
